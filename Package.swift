1a9edd68636601a28016e1f4d894dd349f757205
spvagelis <spirou.vagelis@gmail.com>
2025-02-18 19:18:14 +0200

    Make PaperTrailLumberjack dynamic and changed the url of CocoaAsyncSocket

--- Package.swift
+++ Package.swift
@@ -4,13 +4,23 @@ import PackageDescription
 let package = Package(
     name: "PaperTrailLumberjack",
     products: [
-        .library(name: "PaperTrailLumberjack", targets: ["PaperTrailLumberjack"])
+        .library(
+            name: "PaperTrailLumberjack",
+            type: .dynamic,
+            targets: ["PaperTrailLumberjack"])
     ],
     dependencies: [
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
         .package(url: "https://github.com/spvagelis/CocoaAsyncSocketDynamic.git", branch: "master")
     ],
     targets: [
-        .target(name: "PaperTrailLumberjack", dependencies: ["CocoaLumberjack", "CocoaAsyncSocket"], path: "Classes", publicHeadersPath: "")
+        .target(
+            name: "PaperTrailLumberjack",
+            dependencies: [
+                .product(name: "CocoaLumberjack", package: "CocoaLumberjack"),
+                .product(name: "CocoaAsyncSocket", package: "CocoaAsyncSocketDynamic")
+            ],
+            path: "Classes",
+            publicHeadersPath: "")
     ]
 )
