d73676f4941fadcafb89d94c72ba1a577956140f
spvagelis <spirou.vagelis@gmail.com>
2025-02-18 17:12:23 +0200

    Change the link for CocoaAsyncSocket

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
@@ -7,8 +7,8 @@ let package = Package(
         .library(name: "PaperTrailLumberjack", targets: ["PaperTrailLumberjack"])
     ],
     dependencies: [
-        .package(name: "CocoaLumberjack", url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
-        .package(name: "CocoaAsyncSocket", url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.4")
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.7.0"),
+        .package(url: "https://github.com/spvagelis/CocoaAsyncSocketDynamic.git", branch: "master")
     ],
     targets: [
         .target(name: "PaperTrailLumberjack", dependencies: ["CocoaLumberjack", "CocoaAsyncSocket"], path: "Classes", publicHeadersPath: "")
